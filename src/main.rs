517fa2bef1159a60a3137b41af2d569eac3598c1
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2023-08-31 19:05:26 +0900

    Logger struct to log keys

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 extern crate evdev;
 
-use std::io::{self, Read, Write};
 use evdev::*;
+use std::{
+    fs::File,
+    io::{self, Read, Write},
+};
 
-fn main() -> io::Result<()> {
+fn main() -> Result<(), Errors> {
     let stdin = io::stdin();
     let stdout = io::stdout();
 
@@ -12,6 +15,8 @@ fn main() -> io::Result<()> {
 
     let mut event_buffer = [0u8; std::mem::size_of::<InputEvent>()];
     let mut buffer_offset = 0;
+    let logger =
+        Logger::new("/home/anon/Documents/Code/RustLearning/key_capture/output.txt").unwrap();
 
     loop {
         let mut byte = [0u8; 1];
@@ -24,12 +29,12 @@ fn main() -> io::Result<()> {
 
         if buffer_offset == std::mem::size_of::<InputEvent>() {
             let event: InputEvent = unsafe { std::mem::transmute(event_buffer) };
+            let _ = logger.write_in_file("test");
 
-            if event.event_type() == EventType::KEY && event.code() == Key::KEY_X.code() {
-                let new_event = InputEvent::new(EventType::KEY, Key::KEY_Y.code(), event.value());
-                let new_buffer: [u8; std::mem::size_of::<InputEvent>()] = unsafe {
-                    std::mem::transmute(new_event)
-                };
+            if event.event_type() == EventType::KEY {
+                let new_event = InputEvent::new(EventType::KEY, event.code(), event.value());
+                let new_buffer: [u8; std::mem::size_of::<InputEvent>()] =
+                    unsafe { std::mem::transmute(new_event) };
                 stdout_lock.write_all(&new_buffer)?;
                 stdout_lock.flush()?; // Flush the output immediately
             } else {
@@ -42,4 +47,47 @@ fn main() -> io::Result<()> {
     }
 
     Ok(())
+}
+
+#[derive(Debug)]
+enum Errors {
+    ErrorReadingFile,
+    // Wrapped error from io::error
+    IOError(io::Error),
+}
+impl From<io::Error> for Errors {
+    fn from(e: io::Error) -> Self {
+        Errors::IOError(e)
+    }
+}
+struct Logger {
+    file: File,
+}
+impl Logger {
+    pub fn new(path: &str) -> Result<Logger, Errors> {
+        let file = match File::create(path) {
+            Ok(file) => file,
+            Err(err) => {
+                eprintln!("Error creating file: {}", err);
+                return Err(Errors::ErrorReadingFile);
+            }
+        };
+        return Ok(Logger { file });
+    }
+    pub fn write_in_file(&self, text: &str) -> Result<(), Errors> {
+        writeln!(&self.file, "{}", text)?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn empty() {
+        assert_eq!(2,2);
+    }
+    #[test]
+    fn empty_input() {
+        assert_eq!(1,1);
+    }
 }
