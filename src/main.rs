96cfd118c101906a787892870914a4b6c74a2e87
Noobvelopper <38473290+Noobvelopper@users.noreply.github.com>
2023-08-31 15:28:21 +0900

    Working read and redirect of the keys!!

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,34 @@ fn main() -> io::Result<()> {
     let mut stdin_lock = stdin.lock();
     let mut stdout_lock = stdout.lock();
 
-    let mut buffer = [0u8; std::mem::size_of::<InputEvent>()];
+    let mut event_buffer = [0u8; std::mem::size_of::<InputEvent>()];
+    let mut buffer_offset = 0;
 
-    while stdin_lock.read_exact(&mut buffer).is_ok() {
-        let event: InputEvent = unsafe { std::mem::transmute(buffer) };
+    loop {
+        let mut byte = [0u8; 1];
+        if stdin_lock.read_exact(&mut byte).is_err() {
+            break;
+        }
 
-        if event.event_type() == EventType::KEY && event.code() == Key::KEY_X.code() {
-                let new_event = InputEvent::new(EventType::KEY, Key::KEY_Y.code(), event.value()); 
-            let new_buffer: [u8; std::mem::size_of::<InputEvent>()] = unsafe { std::mem::transmute(new_event) };
-            stdout_lock.write_all(&new_buffer)?;
-        } else {
-            stdout_lock.write_all(&buffer)?;
+        event_buffer[buffer_offset] = byte[0];
+        buffer_offset += 1;
+
+        if buffer_offset == std::mem::size_of::<InputEvent>() {
+            let event: InputEvent = unsafe { std::mem::transmute(event_buffer) };
+
+            if event.event_type() == EventType::KEY && event.code() == Key::KEY_X.code() {
+                let new_event = InputEvent::new(EventType::KEY, Key::KEY_Y.code(), event.value());
+                let new_buffer: [u8; std::mem::size_of::<InputEvent>()] = unsafe {
+                    std::mem::transmute(new_event)
+                };
+                stdout_lock.write_all(&new_buffer)?;
+                stdout_lock.flush()?; // Flush the output immediately
+            } else {
+                stdout_lock.write_all(&event_buffer)?;
+                stdout_lock.flush()?; // Flush the output immediately
+            }
+
+            buffer_offset = 0;
         }
     }
 
